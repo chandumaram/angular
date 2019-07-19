c67bbbd0a5fcf32f2310efa7dbeef16762c0d53b
chandu maram <chandumaram333@gmail.com>
2019-07-19 16:52:29 +0530

    user curd

---
+++ WebApplication1/WebApplication1/Controllers/PersonalDetailsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("Api/PersonalDetails")]
+    public class PersonalDetailsController : ApiController
+    {
+        detailsEntities objEntity = new detailsEntities();
+
+        [HttpGet]
+        [Route("AllDetails")]
+        public IQueryable<PersonalDetail> GetDetails()
+        {
+               return objEntity.PersonalDetails;
+            
+        }
+        [HttpPut]
+        [Route("UpdateDetails")]
+        public IHttpActionResult PutDetails(PersonalDetail personal)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                PersonalDetail obj = new PersonalDetail();
+                obj = objEntity.PersonalDetails.Find(personal.UserId);
+                if (obj != null)
+                {
+                    obj.FirstName = personal.FirstName;
+                    obj.LastName = personal.LastName;
+                    obj.DOB = personal.DOB;
+                    obj.Email = personal.Email;
+                    obj.Village = personal.Village;
+                    obj.State = personal.State;
+                    obj.Country = personal.Country;
+                    obj.FbId = personal.FbId;
+                    obj.TwitterId = personal.TwitterId;
+
+                }
+                int i = this.objEntity.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Ok(personal);
+        }
+    }
+}
