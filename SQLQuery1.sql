390f1ca5c60b7756b3b2ef71450ca65bbfb32bbe
chandumaram <41682282+chandumaram@users.noreply.github.com>
2019-07-19 17:10:56 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+create database details
+use details
+create table PersonalDetails(UserId varchar(30) primary key,FirstName varchar(30),LastName varchar(30),DOB date,Email varchar(30),Village varchar(20),State varchar(15),Country varchar(20),FbId varchar(30),TwitterId varchar(30))
